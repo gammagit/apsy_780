93890904c14fc34287090cc9bcb61034be5220be
Gaurav Malhotra <gmalhotra@albany.edu>
2025-09-08 16:01:50 -0400

    Added in-class mini project coded by Pierce

---
+++ 1_stroop_python_basics/in_class_mini_project/20250907_GroupStroopCode.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  8 13:28:35 2025
+
+@author: johns
+"""
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+
+def generate_rts(n_trials):
+    condition = ['congruent', 'incongruent']
+    trial_decider = np.random.choice(
+        a=condition, size=n_trials, replace=True, p=[.5, .5])
+
+    all_rts = []
+
+    for trial in trial_decider:
+        # rt_congruent = False
+        if trial == "congruent":
+            trial_rt = np.random.normal(400, 100, 1).astype(int)[0]
+        else:
+            trial_rt = np.random.normal(550, 100, 1).astype(int)[0]
+
+        all_rts.append(trial_rt)
+
+    results = {"trial": np.arange(1, n_trials + 1),
+               "condition": trial_decider,
+               "rt": all_rts}
+
+    results_df = pd.DataFrame(results)
+
+    return results_df
+
+
+def analyze_rts(data):
+    mean_congruent = data[data['condition'] == 'congruent'].rt.mean()
+    mean_incongruent = data[data['condition'] == 'incongruent'].rt.mean()
+
+    print(f"Mean of congruent trials: {mean_congruent}")
+    print(f"Mean of incongruent trials: {mean_incongruent}")
+
+
+results = generate_rts(100)
+
+results.to_csv(
+    'C:/Users/johns/OneDrive/Desktop/APSY 780 Computational Methods for AI Models/stroop_data.csv')
+
+
+analyze_rts(results)
+
+sns.boxplot(results, x="condition", y="rt")
